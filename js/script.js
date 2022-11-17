07e353c0187dbc62a57b3e58a25cd747b6f606c0
LSDsloth <lst1788@gmail.com>
2022-11-17 21:49:21 +0100

    Almost done with latest posts javascript

---
+++ js/script.js
@@ -0,0 +1,75 @@
+const postsUrl =
+  "https://libeck.tech/project_exam//wp-json/wp/v2/posts?per_page=2&_embed";
+const postHeader = document.querySelector(".rightSide h3");
+const featuredPost = document.querySelector(".featuredPost");
+const featuredPostsContainer = document.querySelector(".slidesContainer");
+const featuredImageWrapper = document.querySelector(
+  ".featuredPost .imgWrapper"
+);
+
+// for (let i = 0; i < postHeader.length; i++) {
+//   console.log(postHeader[i].innerHTML);
+// }
+
+console.log(postsUrl.length);
+
+async function getPosts(postsUrl) {
+  const response = await fetch(postsUrl);
+  const posts = await response.json();
+  console.log(posts);
+
+  posts.forEach(function (post) {
+    featuredPostsContainer.innerHTML += `
+    <li class="featuredPost">
+              <div class="leftSide">
+                <div
+                  class="imgWrapper"
+                  style="
+                    background: url(${post._embedded["wp:featuredmedia"][0].source_url});
+                    width: 100%;
+                    aspect-ratio: 16 / 9;
+                    background-size: cover;
+                    background-position: 50% 50%;
+                    background-repeat: no-repeat;
+                  "
+                ></div>
+                <div class="categories">
+                  <a class="featured" href="#">${post._embedded["wp:term"][0][0].name}</a>
+                  <a class="news" href="#">News</a>
+                </div>
+              </div>
+
+              <div class="rightSide">
+                <div class="postText">
+                  <h3>${post.title.rendered}</h3>
+                  <p>
+                    ${post.excerpt.rendered}
+                  </p>
+                </div>
+
+                <div class="posterInformation">
+                  <div class="profilepicWrapper">
+                    <a href="#"><img src="${post._embedded.author[0].avatar_urls[24]}" alt="" /></a>
+                  </div>
+
+                  <div class="posterNameAndTitle">
+                    <a href="#">${post._embedded.author[0].name}</a>
+                    <p>Site owner</p>
+                  </div>
+                </div>
+              </div>
+    `;
+  });
+
+  for (let i = 0; i < posts.length; i++) {
+    console.log(posts[i]._embedded);
+    console.log(posts[i].excerpt);
+    console.log(posts[i]);
+  }
+
+  for (let i = 0; i < posts.length; i++) {
+    console.log(posts[i].tags);
+  }
+}
+
+getPosts(postsUrl);
